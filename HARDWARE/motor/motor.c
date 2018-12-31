a7db09c719b1b1304b74e0f64a3289e3589f6756
xjg <473398029@qq.com>
2018-12-31 09:53:42 +0800

    更换电机对应的IO口

--- HARDWARE/motor/motor.c
+++ HARDWARE/motor/motor.c
@@ -6,7 +6,7 @@
 static void INT_GPIO_Init(void)
 {
 	GPIO_InitTypeDef  GPIO_InitStructure;
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);//使能GPIOF时钟
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);//使能GPIOF时钟
 
   //GPIOF9,F10初始化设置
   GPIO_InitStructure.GPIO_Pin = INT1_Pin | INT2_Pin | INT3_Pin| INT4_Pin;
@@ -14,10 +14,10 @@ static void INT_GPIO_Init(void)
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
-  GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化
+  GPIO_Init(GPIOE, &GPIO_InitStructure);//初始化
 
 }
-static void TIM10_PWM_Init(u16 arr,u16 psc)
+static void TIM2_3_PWM_Init(u16 arr,u16 psc)
 {		 					 
 	//此部分需手动修改IO口设置
 	
@@ -25,12 +25,12 @@ static void TIM10_PWM_Init(u16 arr,u16 psc)
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 	TIM_OCInitTypeDef  TIM_OCInitStructure;
 	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM10,ENABLE);  	//TIM10时钟使能    
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);  	//TIM2时钟使能    
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE); 	//使能PORTD时钟	
 	
-	GPIO_PinAFConfig(GPIOB,GPIO_PinSource8,GPIO_AF_TIM10); //GPIOB9复用为定时器11
+	GPIO_PinAFConfig(GPIOB,GPIO_PinSource10,GPIO_AF_TIM2); //GPIOB9复用为定时器11
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;           //GPIOD12
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;           //GPIOD12
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;        //复用功能
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;	//速度100MHz
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;      //推挽复用输出
@@ -42,24 +42,24 @@ static void TIM10_PWM_Init(u16 arr,u16 psc)
 	TIM_TimeBaseStructure.TIM_Period=arr;   //自动重装载值
 	TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1; 
 	
-	TIM_TimeBaseInit(TIM10,&TIM_TimeBaseStructure);//初始化定时器4
+	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseStructure);//初始化定时器4
 	
-	//初始化TIM10 Channel1 PWM模式	 
+	//初始化TIM2 Channel1 PWM模式	 
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //选择定时器模式:TIM脉冲宽度调制模式2
  	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //比较输出使能
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //输出极性:TIM输出比较极性低
-	TIM_OC1Init(TIM10, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM4OC1
+	TIM_OC3Init(TIM2, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM4OC1
 
-	TIM_OC1PreloadConfig(TIM10, TIM_OCPreload_Enable);  //使能TIM10在CCR1上的预装载寄存器
+	TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);  //使能TIM2在CCR1上的预装载寄存器
  
-  TIM_ARRPreloadConfig(TIM10,ENABLE);//ARPE使能 
+  TIM_ARRPreloadConfig(TIM2,ENABLE);//ARPE使能 
 	
-	TIM_Cmd(TIM10, ENABLE);  //使能TIM10
- 
+	TIM_Cmd(TIM2, ENABLE);  //使能TIM2
+//	TIM_CtrlPWMOutputs(TIM2,ENABLE);										  
 										  
 }  
 
-static void TIM11_PWM_Init(u16 arr,u16 psc)
+static void TIM2_4_PWM_Init(u16 arr,u16 psc)
 {		 					 
 	//此部分需手动修改IO口设置
 	
@@ -67,12 +67,12 @@ static void TIM11_PWM_Init(u16 arr,u16 psc)
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 	TIM_OCInitTypeDef  TIM_OCInitStructure;
 	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM11,ENABLE);  	//TIM11时钟使能    
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);  	//TIM2时钟使能    
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE); 	//使能PORTD时钟	
 	
-	GPIO_PinAFConfig(GPIOB,GPIO_PinSource9,GPIO_AF_TIM11); //GPIOB9复用为定时器11
+	GPIO_PinAFConfig(GPIOB,GPIO_PinSource11,GPIO_AF_TIM2); //GPIOB9复用为定时器11
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;           //GPIOD12
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;           //GPIOD12
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;        //复用功能
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;	//速度100MHz
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;      //推挽复用输出
@@ -84,21 +84,20 @@ static void TIM11_PWM_Init(u16 arr,u16 psc)
 	TIM_TimeBaseStructure.TIM_Period=arr;   //自动重装载值
 	TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1; 
 	
-	TIM_TimeBaseInit(TIM11,&TIM_TimeBaseStructure);//初始化定时器4
+	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseStructure);//初始化定时器4
 	
-	//初始化TIM11 Channel1 PWM模式	 
+	//初始化TIM2 Channel1 PWM模式	 
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //选择定时器模式:TIM脉冲宽度调制模式2
  	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //比较输出使能
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //输出极性:TIM输出比较极性低
-	TIM_OC1Init(TIM11, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM4OC1
+	TIM_OC4Init(TIM2, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM4OC1
 
-	TIM_OC1PreloadConfig(TIM11, TIM_OCPreload_Enable);  //使能TIM11在CCR1上的预装载寄存器
+	TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);  //使能TIM2在CCR1上的预装载寄存器
  
-  TIM_ARRPreloadConfig(TIM11,ENABLE);//ARPE使能 
+  TIM_ARRPreloadConfig(TIM2,ENABLE);//ARPE使能 
 	
-	TIM_Cmd(TIM11, ENABLE);  //使能TIM11
- 
-										  
+	TIM_Cmd(TIM2, ENABLE);  //使能TIM2
+//	TIM_CtrlPWMOutputs(TIM2,ENABLE);										  
 }  
 void motor_break(void)
 {
@@ -125,17 +124,22 @@ void motor_reverse(void)
 		INT4 = 0;
 
 }
-void motor_set_value(u16 value)
+void motorL_set_value(u16 value)
 {
-		TIM_SetCompare1(TIM10,value);	//修改比较值，修改占空比
-		TIM_SetCompare1(TIM11,value);	//修改比较值，修改占空比
+		TIM_SetCompare3(TIM2,value);	//修改比较值，修改占空比
+}
+void motorR_set_value(u16 value)
+{
+		TIM_SetCompare4(TIM2,value);	//修改比较值，修改占空比
 }
 void motor_init(void)
 {
 	INT_GPIO_Init();
-	TIM10_PWM_Init(100-1,84-1);
-	TIM11_PWM_Init(100-1,84-1);
+	TIM2_3_PWM_Init(100-1,84-1);
+	TIM2_4_PWM_Init(100-1,84-1);
 	motor_forward();
-	motor_set_value(10-1);
+	motorL_set_value(100-1);
+	motorR_set_value(60-1);
+
 }
 
--- HARDWARE/motor/motor.h
+++ HARDWARE/motor/motor.h
@@ -2,14 +2,14 @@
 #define __MOTOR_H
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_conf.h"
-#define INT1_Pin	GPIO_Pin_7
-#define INT2_Pin	GPIO_Pin_6
-#define INT3_Pin	GPIO_Pin_5
-#define INT4_Pin	GPIO_Pin_15
+#define INT1_Pin	GPIO_Pin_9
+#define INT2_Pin	GPIO_Pin_10
+#define INT3_Pin	GPIO_Pin_11
+#define INT4_Pin	GPIO_Pin_12
 
-#define INT1 PBout(7)	// DS0
-#define INT2 PBout(6)	// DS1
-#define INT3 PBout(5)	// DS0
-#define INT4 PBout(15)	// DS1
+#define INT1 PEout(9)	// DS0
+#define INT2 PEout(10)	// DS1
+#define INT3 PEout(11)	// DS0
+#define INT4 PEout(12)	// DS1
 extern void motor_init(void);
 #endif
